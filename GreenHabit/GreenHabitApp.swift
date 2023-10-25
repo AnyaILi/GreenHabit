be30135e8ccbb9e5d9063ad05b86e8f74fe262e7
Anya <anyali0219@gmail.com>
2023-10-25 01:01:38 -0700

    Initial Commit

---
+++ GreenHabit/GreenHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenHabitApp.swift
+//  GreenHabit
+//
+//  Created by Anya Li on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct GreenHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
